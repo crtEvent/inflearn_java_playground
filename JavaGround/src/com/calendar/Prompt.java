65cec2ca71a425688a9f2100a37e1180aa22d4a2
jshse <jshse@192.168.0.138>
2022-11-12 14:20:35 +0900

    Update Calendar to receive the day of the week
     요일을 입력받아서 해당 요일부터 출력

--- JavaGround/src/com/calendar/Prompt.java
+++ JavaGround/src/com/calendar/Prompt.java
@@ -4,7 +4,33 @@ import java.util.Scanner;
 
 public class Prompt {
 	
-	private final static String PROMPT = "cal>";
+	/**
+	 * 
+	 * @param week 요일명
+	 * @return 0~6 (0=Sunday, 6=Saturday)
+	 */
+	public int parseDay(String week) {
+		
+		switch(week) {
+		case "su": 
+			return 0;
+		case "mo":
+			return 1;
+		case "tu":
+			return 2;
+		case "we":
+			return 3;
+		case "th":
+			return 4;
+		case "fr":
+			return 5;
+		case "sa":
+			return 6;
+		default:
+			return 0;
+		}
+		
+	}
 	
 	public void runPrompt() {
 		Scanner scanner = new Scanner(System.in);
@@ -12,13 +38,19 @@ public class Prompt {
 		
 		int month = -1;
 		int year = -1;
+		int weekday = 0;
 
 		// 월 입력/마지막 날 출력
 		while(true) {
-			System.out.println("년도를 입력하세요.");
+			System.out.println("년도를 입력하세요.(exit: -1)");
 			System.out.print("YEAR> ");
 			year = scanner.nextInt();
-			System.out.println("월을 입력하세요.");
+			
+			if(year == -1) {
+				break;
+			}
+			
+			System.out.println("월을 입력하세요.(exit: -1)");
 			System.out.print("MONTH> ");
 			month = scanner.nextInt();
 			
@@ -27,10 +59,16 @@ public class Prompt {
 			}
 			
 			if(month > 12 || month < -1) {
+				System.out.println("잘못된 값을 입력하였습니다.");
 				continue;
 			}
 			
-			cal.printCalendar(year, month);
+			System.out.println("첫째 날의 요일을 입력하세요.(su mo tu we th fr sa)");
+			System.out.print("WEEKDAY> ");
+			String str_weekday = scanner.next();
+			weekday = parseDay(str_weekday);
+			
+			cal.printCalendar(year, month, weekday);
 		}
 		
 		System.out.println("Bye~");
