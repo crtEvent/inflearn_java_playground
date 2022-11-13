5e715036c716b1e248aa6b4a88f4d37190db1601
crtEvent <crtevn@gmail.com>
2022-11-14 00:56:22 +0900

    Feat: Implement prompt
     명령을 입력 받는 프롬프트 구현

--- JavaGround/src/com/crtevn/gugudan/Prompt.java
+++ JavaGround/src/com/crtevn/gugudan/Prompt.java
@@ -1,5 +1,7 @@
 package com.crtevn.gugudan;
 
+import java.util.Scanner;
+
 public class Prompt {
 	
 	// [계획]
@@ -10,5 +12,47 @@ public class Prompt {
 	// 4-1. 2*1 부터 n*n 까지 계산(nn단)
 	// 4-2. 2*1 부터 n*m 까지 계산(nm단)
 	// 4. 사용자가 종료할 때 까지 반복
+	
+	public void runPrompt() {
+		System.out.println("2 이상의 하나의 정수 혹은 쉼표로 구분되는 두 자리 정수를 입력하세요.");
+		System.out.println("예) '8' or '8,7', 종료: q");
+		Scanner scanner = new Scanner(System.in);
+		
+		boolean isLoop = true;
+		while(isLoop) {
+			String inputValue = scanner.nextLine();
+			
+			if(inputValue.equals("q")){
+				System.out.println("종료합니다.");
+				isLoop = false;
+				break;
+			}
+			
+			String[] splitedValue = inputValue.split(",");
+			
+			if (splitedValue.length == 1) {
+				int firstInt = Integer.parseInt(splitedValue[0]);
+				System.out.printf("%d*%d 까지 계산합니다.\n", firstInt, firstInt);
+				// -> nn단 계산
+			}
+			
+			if (splitedValue.length == 2) {
+				int firstInt = Integer.parseInt(splitedValue[0]);
+				int secondInt = Integer.parseInt(splitedValue[1]);
+				System.out.printf("%d*%d 까지 계산합니다.\n", firstInt, secondInt);
+				// -> nm단 계산
+			}
+			
+		}
+		
+		scanner.close();
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		Prompt p = new Prompt();
+		p.runPrompt();
+	}
 	
 }
